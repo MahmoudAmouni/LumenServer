3dfe871397a0e2810473853ff3f9e5d63b9e4927
mhdzz1 <110099308+mhdzz1@users.noreply.github.com>
2025-12-09 09:26:36 +0200

    Updated candidates-migrations

--- database/migrations/2025_12_08_093551_candidates.php
+++ database/migrations/2025_12_08_093551_candidates.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone_number')->nullable();
             $table->string('level');
-            $table->string('portfolio_url')->nullable();
+            $table->string('github_url')->nullable();
             $table->integer('age')->nullable();
             $table->string('location')->nullable();
              $table->text('location')->nullable();
