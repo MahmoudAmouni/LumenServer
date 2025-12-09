51295c33c9ac3a0be000d72cc12192170b7e2811
mhdzz1 <110099308+mhdzz1@users.noreply.github.com>
2025-12-09 17:30:35 +0200

    Updated offer model

--- app/Models/offer.php
+++ app/Models/offer.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class offer extends Model
 {
-    //
+ protected $fillable = [
+        'candidate_id',
+        'base_salary',
+        'bonus',
+        'start_date',
+        'contract_type',
+        'offer_letter_template',
+        'status',
+    ];
+
+    public function candidate()
+    {
+        return $this->belongsTo(Candidate::class, 'candidate_id');
+    }
+
 }
