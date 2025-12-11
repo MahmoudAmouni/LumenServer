61baabb1e236a568314f19e3e2594c292ab73aa7
mhdzz1 <110099308+mhdzz1@users.noreply.github.com>
2025-12-11 09:26:18 +0200

    Updated Done

--- database/migrations/2025_12_08_093501_users.php
+++ database/migrations/2025_12_08_093501_users.php
@@ -10,8 +10,10 @@ return new class extends Migration {
      */
     public function up(): void
     {
-            Schema::create('users', function (Blueprint $table) {
+            Schema::create('users', function (Blueprint $table) { 
+              
               $table->id();
+              
             $table->integer('company_id')->nullable();  
             $table->integer('type_id');
             $table->string('name');
